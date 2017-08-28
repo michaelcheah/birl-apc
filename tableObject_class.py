c03657eec7992a467142acfea2a5af90c4259ea9
Michael Cheah <michaelcheah95@gmail.com>
2017-08-28 11:35:29 +0100

    TableObject Class updated
    Added show figure option and removed 'nuclear' from return object attributes

--- tableObject_class.py
+++ tableObject_class.py
@@ -13,11 +13,8 @@ import kinect_vision as kv
 from kinect_vision import PATH_TO_KINECT_IMAGES_DIR
 
 class TableObject(object):
-    
+
     def __init__(self, member=None, family=None, rgb_member=None, name="Unknown"):
-        
-        
-        
         if member is not None:
             self.contour = member['contour']
             self.height = member['height']
@@ -32,54 +29,54 @@ class TableObject(object):
             self.box = self.extract_minBox(self.contour)
             self.centre, self.radius = TableObject.extract_minCircle(self,self.contour)
             self.area = TableObject.find_objectArea(self, self.contour)
-        
+
         elif rgb_member is not None:
             self.height = (0, 0, 0, 0)
             self.children = []
             self.generation = 0
             self.name = name
-        
+
         else:
             print "This object does not exist"
-        
+
         if rgb_member is not None:
             self.item_contour = [rgb_member['contour']]
-            
+
             self.rgb_box = TableObject.extract_minBox(self, self.item_contour)
             self.rgb_centre, self.rgb_radius = TableObject.extract_minCircle(self, self.item_contour)
             self.rgb_area = TableObject.find_objectArea(self, self.item_contour)
-        
+
         TableObject.prepare_data(self)
-        
+
     def find_children(self,family):
         children_list = []
         for child in self.children:
             children_list.append(family[child])
         self.children_info = children_list
-    
+
     def return_nuclear(self, member, family):
         nuclear = [member]
         for i in member['children']:
             nuclear.append(family[i])
             print family[i]['id']
         return nuclear
-    
+
     def extract_minBox(self, contour):
         box = cv2.minAreaRect(contour[0])
         box = cv2.boxPoints(box)
         box = np.array(box, dtype="int")
         return box
-    
+
     def extract_minCircle(self, contour):
         centre, radius = cv2.minEnclosingCircle(contour[0])
         return centre, radius
-        
+
     def draw_minBox(self, image, box):
         img = copy.copy(image)
         cv2.drawContours(img, [box], -1, (255,0,0), 1)
         plt.figure()
         plt.imshow(img)
-        
+
     def draw_minCircle(self, image, centre, radius):
         img = copy.copy(image)
         cent = (int(centre[0]), int(centre[1]))
@@ -87,28 +84,28 @@ class TableObject(object):
         cv2.circle(img, cent, radi, (0,255,0), 1)
         plt.figure()
         plt.imshow(img)
-    
+
     def find_objectArea(self, contour):
         area = cv2.contourArea(contour[0])
         return area
-    
+
     def find_objectPerimeter(self, contour):
         perimeter = cv2.arcLength(contour[0],True)
         return perimeter
-    
+
     def print_children_attributes(self):
         print ("Children Info:")
         for i,child in enumerate(self.children_info):
             print (i, child['generation'], child['children'],child['height'])
-            
+
     def print_object_attributes(self, children_info=False):
-        object_attr = [attr for attr in dir(self) 
-                       if not callable(getattr(self, attr)) 
+        object_attr = [attr for attr in dir(self)
+                       if not callable(getattr(self, attr))
                        and not attr.startswith("__")]
         for attr in object_attr:
-            if attr != 'children_info':
+            if attr != 'children_info' and attr != "nuclear":
                 print attr, ": ", getattr(self, attr)
-                
+
     def find_rgb_average(self, image):
         out = TableObject.filter_object(self, image)
         try:
@@ -116,7 +113,7 @@ class TableObject(object):
         except:
             contour = self.contour
         img = copy.copy(image)
-        
+
         if len(np.shape(out))>2:
             mask = vt.create_contour_mask(contour, img[:,:,0])
             r_avg = np.mean(img[:,:,0][mask==255])
@@ -126,17 +123,17 @@ class TableObject(object):
         else:
             mask = vt.create_contour_mask(contour, img)
             np.mean(img[mask==255])
-    
+
     def return_object_attributes(self, children_info=False):
-        object_attr = [attr for attr in dir(self) 
-                       if not callable(getattr(self, attr)) 
+        object_attr = [attr for attr in dir(self)
+                       if not callable(getattr(self, attr))
                        and not attr.startswith("__")]
         object_dict = {}
         for attr in object_attr:
-            if attr != 'children_info' or attr != "nuclear":
+            if attr != 'children_info' and attr != "nuclear":
                 object_dict[attr]=getattr(self, attr)
         return object_dict
-        
+
     def filter_object(self, image, show_contour = False):
         try:
             contour = self.item_contour
@@ -148,23 +145,31 @@ class TableObject(object):
         img = copy.copy(image)
         obj_mask = vt.create_contour_mask(contour, img)
         out = np.zeros_like(image)
-        
+
         if show_contour:
-            cv2.drawContours(img, [self.contour], -1, (255,255,255), 1)
+            cv2.drawContours(img, contour, -1, (255,255,255), 1)
         out[obj_mask==255] = img[obj_mask==255]
         return out
-    
-    def label_object(self, image):
+
+    def label_object(self, image, name=None, show=True):
         out = TableObject.filter_object(self, image)
+        if show:
+            plt.figure(str(self.name))
+            plt.imshow(out)
+            plt.show()
+        if name is None:
+            name = raw_input("Name of object?: ")
+        self.name = name
+
+    def display_object(self, image, name=None, show_contour=True):
+        out = TableObject.filter_object(self, image, show_contour=show_contour)
         plt.figure(str(self.name))
         plt.imshow(out)
         plt.show()
-        name = raw_input("Name of object?: ")
-        self.name = name
-    
+
     def add_depth(self, depth_object):
         try:
-            
+
             self.contour = depth_object.contour
             self.height = depth_object.height
             self.children = depth_object.children
@@ -183,14 +188,14 @@ class TableObject(object):
             self.aspect = depth_object.aspect
             self.circularness = depth_object.circularness
             self.fill = depth_object.fill
-            
+
         except Exception as ex:
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print "There is no depth data to be added"
             print message
-    
-    
+
+
     def prepare_data(self):
         try:
             # prepare aspect ratio
@@ -211,13 +216,13 @@ class TableObject(object):
             rgb_boundCircleArea = np.pi*(self.rgb_radius)**2
             rgb_circularness = self.rgb_area/rgb_boundCircleArea
             self.rgb_circularness = rgb_circularness
-            
+
         except Exception as ex:
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print "RGB data not considered"
             print message
-    
+
         try:
             # FILL
             boundBoxArea = cv2.contourArea(self.box)
@@ -237,13 +242,13 @@ class TableObject(object):
             boundCircleArea = np.pi*(self.radius)**2
             circularness = self.area/boundCircleArea
             self.circularness = circularness
-            
+
         except Exception as ex:
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print "Depth data not implemented"
             print message
-            
+
     def label_data_auto(self):
         if self.height[0] == 0:
             self.name = 'cd'
@@ -256,7 +261,7 @@ def find_object(image, family=None, rgb_family=None, label=True):
         object_id = 1
     img = copy.copy(image)
     if rgb_family is not None:
-        curr_family = rgb_family  
+        curr_family = rgb_family
     elif family is not None:
         curr_family = family
     else:
@@ -268,8 +273,8 @@ def find_object(image, family=None, rgb_family=None, label=True):
                 box = cv2.boxPoints(box)
                 box = np.array(box, dtype="int")
                 cv2.drawContours(img, [box], -1, (255,0,0), 1)
-            
-            
+
+
                 if rgb_family is None:
                     tobj = TableObject(member = obj, family = family)
                 else:
@@ -283,11 +288,11 @@ def find_object(image, family=None, rgb_family=None, label=True):
                     object_id = object_id+1
 
     return Object_List
-        
+
 def match_rgb_with_depth(family, rgb_family, image, rgb_image, label=True):
     img = copy.copy(image)
     rgb_img = copy.copy(rgb_image)
-    
+
     if label:
         depth_obj_list = find_object(family=family, image=img, label=label)
         obj_list = find_object(rgb_family=rgb_family, image=rgb_img, label=label)
@@ -307,7 +312,7 @@ def match_rgb_with_depth(family, rgb_family, image, rgb_image, label=True):
 def match_rgb_with_depth_v2(family, rgb_family, image, rgb_image, label=False):
     img = copy.copy(image)
     rgb_img = copy.copy(rgb_image)
-    
+
     if label:
         depth_obj_list = find_object(family=family, image=img, label=label)
         obj_list = find_object(rgb_family=rgb_family, image=rgb_img, label=label)
@@ -334,8 +339,8 @@ def match_rgb_with_depth_v2(family, rgb_family, image, rgb_image, label=False):
                 inside = 0
 
                 for point in depth_contour:
-                    check = cv2.pointPolygonTest((contour), 
-                                                           (point[0][0],point[0][1]), 
+                    check = cv2.pointPolygonTest((contour),
+                                                           (point[0][0],point[0][1]),
                                                            measureDist=True)
                     if check>0:
                         inside = inside+1
@@ -353,7 +358,7 @@ def match_rgb_with_depth_v2(family, rgb_family, image, rgb_image, label=False):
                 mask2 = vt.create_contour_mask([depth_list[most_likely_depth].contour[0]], img)
                 mask = mask-mask2
                 obj_list[item].add_depth(depth_list[most_likely_depth])
-        
+
         print "Matched based on Contour Overlap"
 
     return obj_list
